40ce2241acc534502deeeac61ce95277599c449f
patw <patw.hi@gmail.com>
2016-03-10 18:25:15 +0800

    Added an api for test

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,12 @@
 from todo import app
-from flask import render_template
+from flask import render_template, jsonify
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/api/test/')
+def test():
+    return jsonify(result='ok')
