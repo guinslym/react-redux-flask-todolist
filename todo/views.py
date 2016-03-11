96eee713f780c72bea4539bebeaace06cb70fcf4
patw <patw.hi@gmail.com>
2016-03-11 17:39:37 +0800

    Backend GET, PUT and DELETE api

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,9 @@
+import os
+import io
+import json
+
 from todo import app
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 
 
 @app.route('/')
@@ -7,6 +11,52 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/api/test/')
-def test():
-    return jsonify(result='ok')
+@app.route('/api/todos/', methods=['GET'])
+def todos_list():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    JSON_URL = os.path.join(SITE_ROOT, 'static', 'todos.json')
+
+    with open(JSON_URL) as data_file:
+        json_data = json.load(data_file)
+
+    return jsonify(result='ok', data=json_data['todos'])
+
+
+@app.route('/api/todos/', methods=['PUT'])
+def todos_update():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    JSON_URL = os.path.join(SITE_ROOT, 'static', 'todos.json')
+
+    data_dict = json.loads(request.data)
+    id = data_dict['id']
+    title = data_dict['title']
+
+    with open(JSON_URL) as data_file:
+        json_data = json.load(data_file)
+
+    json_data['todos'].append({'id': id, 'title': title})
+
+    with io.open(JSON_URL, 'w', encoding='utf-8') as f:
+        f.write(unicode(json.dumps(json_data, ensure_ascii=False)))
+
+    return jsonify(result='ok', data=json_data)
+
+
+@app.route('/api/todos/', methods=['DELETE'])
+def todos_remove():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    JSON_URL = os.path.join(SITE_ROOT, 'static', 'todos.json')
+
+    data_dict = json.loads(request.data)
+    id = data_dict['id']
+
+    with open(JSON_URL) as data_file:
+        json_data = json.load(data_file)
+
+    json_data['todos'] = \
+        [x for x in json_data['todos'] if not (id == x.get('id'))]
+
+    with io.open(JSON_URL, 'w', encoding='utf-8') as f:
+        f.write(unicode(json.dumps(json_data, ensure_ascii=False)))
+
+    return jsonify(result='ok', data=json_data)
